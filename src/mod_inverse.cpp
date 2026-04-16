a07d1852f8427ad6d579b78990a6994ecdb448e0
NguyenGiaHuan# <huankaka2006@gmail.com>
2026-04-16 14:22:38 +0700

    Update mod_inverse.cpp

--- src/mod_inverse.cpp
+++ src/mod_inverse.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+
 using namespace std;
 
 int gcd(int a, int b) {
@@ -26,11 +27,14 @@ int extended_euclid(int a, int b, int &x, int &y) {
 }
 
 int mod_inverse(int a, int m) {
-    // TODO(student): implement modular inverse using extended_euclid()
-    // If inverse does not exist, return -1.
+    int x = 0, y = 0;
+    int g = extended_euclid(a, m, x, y);
     (void)a;
     (void)m;
-    return -1;
+    if (g == 1) {
+        return (x % m + m) % m;
+    }
+    return ~0;
 }
 
 int main() {
